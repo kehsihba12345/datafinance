ab5f3fc8c335f5e50d0969ca138a42d58ba1f6e9
Abhishek Gautam <abhishek.gautam642@gmail.com>
2023-02-02 20:35:31 +0530

    updated hero font for small screen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
 		<div className='text-white'>
 			<div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
 				<p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
-				<h1 className='md:text-7xml sm:text-6xl text-4xml font-bold md:py-6'>Grow with data.</h1>
+				<h1 className='md:text-7xml sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
 				<div className='flex justify-center items-center'>
 					<p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, flexible financing for</p>
 					<Typed
